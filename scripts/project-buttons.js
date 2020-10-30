aea3a9b0758b15631a80d228797f6c645bfa5c97
KishorS03 <656268@pdsb.net>
2020-10-30 17:01:22 -0400

    Buttons 1 - 6 are complete

--- scripts/project-buttons.js
+++ scripts/project-buttons.js
@@ -1,11 +1,14 @@
+// Button 1
 function theTime() {
 	document.getElementById('time').innerHTML = Date();
 }
 
+// Button 2
 function theClicker() {
 	document.getElementById('button2').innerHTML = "I was clicked!";
 }
 
+// Button 3
 function toggleText() {
 	var x = document.getElementById("toggle");
 	if (x.innerHTML === "Swap Text") {
@@ -15,3 +18,24 @@ function toggleText() {
 	}
 }
 
+// Button 4
+function alertp4() {
+	alert("Project 1 is coming soon!");
+}
+
+// Button 5
+function sportsQ() {
+  var txt;
+  var sport = prompt("What is your favorite sport?:", " basketball");
+  if (sport == null || sport == "") {
+    txt = "User cancelled the prompt.";
+  } else {
+    txt = "My favorite sport is" + sport;
+  }
+  document.getElementById("sport").innerHTML = txt;
+}
+
+//Button 6 - Alert
+function alert6() {
+	alert("Project 2 will be coming soon");
+}
