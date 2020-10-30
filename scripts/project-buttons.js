a0160f086db9ab014c66955a9506b8e47f92881c
KishorS03 <71361655+KishorS03@users.noreply.github.com>
2020-10-30 09:52:42 -0400

    Secomd Button test

--- scripts/project-buttons.js
+++ scripts/project-buttons.js
@@ -1,3 +1,8 @@
 function theTime() {
 	document.getElementById('time').innerHTML = Date();
 }
+
+document.getElementById('button2').onclick = function() {myFunction()};
+function myFunction() {
+  document.getElementById('button2').innerHTML = "YOU CLICKED ME!";
+}
