b7a8556d93a615b3a277010a9f4eac464b25fb81
KishorS03 <656268@pdsb.net>
2020-10-29 20:58:53 -0400

    Im confused

--- scripts/project-buttons.js
+++ scripts/project-buttons.js
@@ -1,1 +1,5 @@
-document.getElementByClassName("button1")
+document.getElementByClassName("button1").onclick = function() {myFunction()};
+
+function myFunction() {
+  document.getElementById("button1").innerHTML = "YOU CLICKED ME!";
+}
