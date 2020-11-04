1336e3fa9c299b0119fe569aacff9b73be059e9e
KishorS03 <71361655+KishorS03@users.noreply.github.com>
2020-11-04 10:07:14 -0500

    Exercise 7

--- scripts/project-buttons.js
+++ scripts/project-buttons.js
@@ -166,3 +166,10 @@ function tenFour() {
 	 		}
 	 } 
 }
+
+// Create a JavaScript function, centuryFromYear(), that, given a year, returns the century it is in. 
+// The first century spans from the year 1 up to and including the year 100, 
+// The second - from the year 101 up to and including the year 200, etc. For example:
+// centuryFromYear(1705) returns 18, centuryFromYear(1900) returns 19, centuryFromYear(1601) returns 17, centuryFromYear(2000) returns 20
+function centuryFromYear() {
+}
